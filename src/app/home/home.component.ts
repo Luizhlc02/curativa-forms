4e7cdf4c4f410296ba92da3bb18dacb39b7c53aa
Luiz Henrique <luizhlc02@gmail.com>
2025-06-08 21:22:51 -0300

    feat: criando menu e melhorando nav bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { MenuComponent } from "./menu/menu.component";
 
 @Component({
   selector: 'app-home',
   imports: [
-    NavBarComponent
-  ],
+    NavBarComponent,
+    MenuComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/home/menu/menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TabsModule } from 'primeng/tabs';
+
+@Component({
+  selector: 'app-menu',
+  imports: [
+    TabsModule
+  ],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+}
